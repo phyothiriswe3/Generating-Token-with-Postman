5365c69193570ff396e6176f1f2a033e271a15b4
Phyo Thiri Swe <52002333+phyothiriswe3@users.noreply.github.com>
2023-03-08 15:24:35 +0630

    Add files via upload

---
+++ Repository/IJWTManagerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing_Authentication_Project.Models;
+
+namespace Testing_Authentication_Project.Repository
+{
+    public interface IJWTManagerRepository
+    {
+        Tokens Authenticate(Users users);
+    }
+}
