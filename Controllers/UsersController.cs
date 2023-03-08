45c556e995ab815a99cb15461e0677eca2feafa5
Phyo Thiri Swe <52002333+phyothiriswe3@users.noreply.github.com>
2023-03-08 15:17:42 +0630

    Controllers

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing_Authentication_Project.Models;
+using Testing_Authentication_Project.Repository;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Testing_Authentication_Project.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IJWTManagerRepository _jWTManager;
+
+        public UsersController(IJWTManagerRepository jWTManager)
+        {
+            this._jWTManager = jWTManager;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public List<string> Get()
+        {
+            var users = new List<string>
+        {
+            "Satinder Singh",
+            "Amit Sarna",
+            "Davin Jon"
+        };
+
+            return users;
+        }
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UsersController>
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("authenticate")]
+        public IActionResult Authenticate(Users usersdata)
+        {
+            var token = _jWTManager.Authenticate(usersdata);
+
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(token);
+        }
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
