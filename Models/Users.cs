f17b69c9fb7edaa9f3c0579509daed208e4c3e58
Phyo Thiri Swe <52002333+phyothiriswe3@users.noreply.github.com>
2023-03-08 15:18:34 +0630

    Models

---
+++ Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing_Authentication_Project.Models
+{
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
